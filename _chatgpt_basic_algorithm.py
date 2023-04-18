956d9ec1304427dff4053687d55af7bad482699e
hasan-furkan <hsnfrkn32@gmail.com>
2023-04-18 16:36:19 +0300

    chatgpt onerisi ile basit seviyede algoritma sorulari

---
+++ _chatgpt_basic_algorithm.py
@@ -0,0 +1,24 @@
+# Girilen bir sayının karesini bulan bir program yazın.
+
+# Verilen bir listedeki en büyük sayıyı bulan bir program yazın.
+
+# Verilen bir cümledeki harflerin frekansını hesaplayan bir program yazın.
+
+# Verilen bir sayı listesindeki tüm sayıları toplayan bir program yazın.
+
+# Verilen bir sayı listesindeki en küçük sayıyı bulan bir program yazın.
+
+# Verilen bir sayı listesindeki çift sayıları bulan bir program yazın.
+
+# Verilen bir sayı listesindeki tek sayıları bulan bir program yazın.
+
+# Verilen bir kelimeyi tersten yazdıran bir program yazın.
+
+# Verilen bir sayı listesindeki sayıların ortalamasını bulan bir program yazın.
+
+# Verilen bir sayı listesindeki sayıların toplamının kaç adet olduğunu bulan bir program yazın.
+
+#### 1 ####
+n = input('bir sayi girin')
+print(int(n) * int(n))
+#### 1 ####
